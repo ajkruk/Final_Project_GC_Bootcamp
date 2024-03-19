2bb3bf8dfdc514de92174b5704e6d902f49a59ee
Sarah Sokoll <sarahmsokoll@gmail.com>
2024-03-18 21:34:07 -0400

    3/18 9:34p

--- Book_Trade_FinalProject/frontend/src/components/Book.tsx
+++ Book_Trade_FinalProject/frontend/src/components/Book.tsx
@@ -4,19 +4,22 @@ import './Book.css';
 interface BookProps {
     title: string;
     author: string;
-    imageURL: string;
-    additionalInfo?: any; // replace 'any' with a more specific type as it comes up
+    genre: string;
+    imageURL: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fgifts.worldwildlife.org%2Fgift-center%2Fgifts%2FSpecies-Adoptions%2FEmperor-Penguin.aspx&psig=AOvVaw3FZXdRW09-ybDGZvuOngbb&ust=1710896884955000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiiotqR_4QDFQAAAAAdAAAAABAE'; // will typically call from api or user-uploaded image
+    // add a section for the quality of the book? that might be too much
 }
 
-const Book: React.FC<BookProps> = ({ title, author, imageUrl, additionalInfo }) => {
+const Book: React.FC<BookProps> = ({ title, author, genre, imageUrl }) => {
     return (
         <div className="book">
-            <img src={https://www.google.com/url?sa=i&url=https%3A%2F%2Fgifts.worldwildlife.org%2Fgift-center%2Fgifts%2FSpecies-Adoptions%2FEmperor-Penguin.aspx&psig=AOvVaw3FZXdRW09-ybDGZvuOngbb&ust=1710896884955000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMiiotqR_4QDFQAAAAAdAAAAABAE} alt={'Cover of ${title}'} className="book-image" />
+            <img src={imageUrl} alt={'Cover of ${title}'} className="book-image" />
             <div className="book-info">
                 <h3 className="book-title">{title}</h3>
                 <p className="book-author">{author}</p>
+                <p className="book-genre">{genre}</p>
             </div>
         </div>
     );
 };
 
+export default Book;
