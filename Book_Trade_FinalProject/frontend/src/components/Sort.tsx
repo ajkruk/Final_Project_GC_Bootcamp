57e1afe41253681b24566baab66f314ac315261e
Sarah Sokoll <sarahmsokoll@gmail.com>
2024-03-24 19:59:25 -0400

    api, search form, and results

--- Book_Trade_FinalProject/frontend/src/components/Sort.tsx
+++ Book_Trade_FinalProject/frontend/src/components/Sort.tsx
@@ -6,37 +6,31 @@ const Sort = () => {
         {
             title: "Indigo Ridge",
             author: "Devney Perry",
-            genre: "Modern Romance",
             imageUrl: "https://m.media-amazon.com/images/I/71VeFEmBSsL._AC_UF1000,1000_QL80_.jpg"
         },
         {
             title: "Juniper Hill",
             author: "Devney Perry",
-            genre: "Modern Romance",
             imageUrl: "https://m.media-amazon.com/images/I/510MOAGJzTL.jpg"
         },
         {
             title: "Garnet Flats",
             author: "Devney Perry",
-            genre: "Modern Romance",
             imageUrl: "https://m.media-amazon.com/images/I/511XMMX5VVL.jpg"
         },
         {
             title: "Jasper Vale",
             author: "Devney Perry",
-            genre: "Modern Romance",
             imageUrl: "https://m.media-amazon.com/images/I/512Fdj8iBGL.jpg"
         },
         {
             title: "Crimson River",
             author: "Devney Perry",
-            genre: "Modern Romance",
             imageUrl: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1672870661i/75579822.jpg"
         },
         {
             title: "Sable Peak",
             author: "Devney Perry",
-            genre: "Modern Romance",
             imageUrl: "https://m.media-amazon.com/images/I/51JpFAo1exL.jpg"
         }
     ]
@@ -74,7 +68,7 @@ const Sort = () => {
     
     return <div>
         {edensSeries.map(book => (
-                    <Book title={book.title} author={book.author} genre={book.genre} imageUrl={book.imageUrl}></Book>
+                    <Book title={book.title} author={book.author} imageUrl={book.imageUrl}></Book>
         ))}
     </div>
 }
