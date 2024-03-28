f2f55a16c1c4f0e922cb494fe3ec2bdc2f37c2f5
ajkruk <alicia.j.kruk@gmail.com>
2024-03-27 20:47:05 -0400

    fourth time

--- Book_Trade_FinalProject/frontend/src/App.tsx
+++ Book_Trade_FinalProject/frontend/src/App.tsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React, { useState } from 'react';
-import LoginPage from './components/LoginPage';
-import MyCollection from './components/MyCollection';
-import { NewUserForm } from './components/NewUserForm';
-import UserCard from './components/UserCard';
-import ChooseCollection from './components/ChooseCollection';
-import SearchForm from './components/SearchForm';
-import ViewUserCollection from './components/ViewUserCollection';
-import MainPage from './components/MainPage';
-
-const App: React.FC = () => {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    // Perform your login logic here
-    // For demonstration purposes, let's consider login successful if user clicks on login button
-    setIsLoggedIn(true);
-  };
-=======
 import React from 'react';
 import { BrowserRouter, Route, Routes, Link, NavLink} from 'react-router-dom';
 import LoginPage from './components/LoginPage';
@@ -30,26 +10,9 @@ import Sort from './components/Sort';
 // import SearchForm from './components/SearchForm';
 // import NewBook from './components/BookForm';
 // import Book from './components/Book';
->>>>>>> e2016509ca1908ae5ea5d0dd88e9cc437d8664b2
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div>
-      <MainPage children={undefined}/> 
-      {<LoginPage onLogin={function (): void {
-        throw new Error('Function not implemented.');
-      } }/>}
-      <NewUserForm/>
-      <UserCard firstName={''} lastName={''} userName={''} email={''} password={''} image={''}/>
-      <MyCollection/>
-      <ChooseCollection/>
-      <ViewUserCollection/>
-      <SearchForm/> 
-
-
-    </div>
-=======
     <BrowserRouter>
       <header>
         <nav>
@@ -72,7 +35,6 @@ function App() {
         </Routes>
       </main>
     </BrowserRouter>
->>>>>>> e2016509ca1908ae5ea5d0dd88e9cc437d8664b2
   );
 }
 
