e820a9929c7252b1367a33872c162d519d6ef936
Juan R. Linares Orihuela <122564427+JuanLinaresOrihuela@users.noreply.github.com>
2024-03-31 19:41:55 -0400

    trying to route

--- Book_Trade_FinalProject/frontend/src/App.tsx
+++ Book_Trade_FinalProject/frontend/src/App.tsx
@@ -1,40 +1,35 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes, Link, NavLink} from 'react-router-dom';
+import { 
+  createBrowserRouter, 
+  Route, 
+  Routes,
+  createRoutesFromElements,
+  RouterProvider} from 'react-router-dom';
+
+
+// Pages to import.
+
 import LoginPage from './components/LoginPage';
-import { NewUserForm } from './components/NewUserForm';
-import ChooseCollection from './components/ChooseCollection';
-import UserCard from './components/UserCard';
+// import { NewUserForm } from './components/NewUserForm';
+// import ChooseCollection from './components/ChooseCollection';
+// import UserCard from './components/UserCard';
 import MainPage from './components/MainPage';
-import Sort from './components/Sort';
+// import Sort from './components/Sort';
 // import SearchResults from './components/SearchResults';
 // import SearchForm from './components/SearchForm';
 // import NewBook from './components/BookForm';
 // import Book from './components/Book';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+        <Route path='/' element={RootLayout}>
+          <Route index element={<LoginPage />} />
+        </Route>
+  )
+);
+
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <NavLink to='/'>Log In</NavLink>
-          <NavLink to='MainPage'>Main</NavLink>
-          <NavLink to='SignUp'>Sign Up</NavLink>  
-          <NavLink to='ChooseCollection'>Choose Collection</NavLink>
-          <NavLink to='UserCard'>User Card</NavLink>
-          <NavLink to='Sort'>Sort</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          <Route index element={<LoginPage />} />
-          <Route path='Welcome' element={<MainPage />} />
-          <Route path='SignUp' element={<NewUserForm/>}/>
-          <Route path='ChooseCollection' element={<ChooseCollection/>}/>
-          <Route path='UserCard' element={<UserCard firstName={''} lastName={''} userName={''} email={''} password={''} image={''}/>}/>
-          <Route path='Sort' element={<Sort/>}/>
-        </Routes>
-      </main>
-    </BrowserRouter>
+      <RouterProvider router={router} />
   );
 }
 
